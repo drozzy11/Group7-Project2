ec9fbbd71ef2ec35c37ce926b073fa107e4ce2c9
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-06-28 21:03:28 -0500

    removed negative z index from background container properties to reinstate hover and click properties

--- public/assets/index.js
+++ public/assets/index.js
@@ -16,3 +16,7 @@ $(function() {
 document.querySelector('#playlistBtn','.login-btn').addEventListener('click', () => {
   document.querySelector('#playlistBtn','.login-btn').classList.add('.homeOnCLick');
 }); 
+
+document.querySelector('.micIcon').addEventListener('click', () => {
+  document.querySelector('.navbar-collapse').classList.toggle('show');
+});
