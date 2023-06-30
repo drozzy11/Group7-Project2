63c21f714f757811de86688ad9913a37984610ab
drozzy11 <ruiz.diego071299@gmail.com>
2023-06-29 21:45:05 -0500

    database can now hold song data

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+// server.js
+
 const express = require('express');
 const app = express();
 const api = require('./api');
+const sequelize = require('./config/connection');
+const { insertSongs } = require('./database');
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
@@ -8,7 +12,6 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/playlist.html');
 });
-
 
 app.use(express.json());
 
@@ -20,14 +23,29 @@ app.get('/callback', (req, res) => {
     return;
   }
 
-  api.searchSongsByGenre(genre, popularity, numSongs)
-    .then(function (tracks) {
-      console.log("Tracks:", tracks); // Add this line for debugging
-      res.json({ tracks });
+  sequelize.sync({ force: true })
+    .then(() => {
+      api.searchSongsByGenre(genre, popularity, numSongs)
+        .then(function (tracks) {
+          console.log("Tracks:", tracks);
+
+          insertSongs(tracks.tracks)
+            .then(() => {
+              res.json({ tracks });
+            })
+            .catch(function (err) {
+              console.log('Error inserting songs:', err);
+              res.status(500).json({ error: 'Error inserting songs into the database' });
+            });
+        })
+        .catch(function (err) {
+          console.log('Something went wrong!', err);
+          res.status(500).json({ error: 'Error generating playlist' });
+        });
     })
-    .catch(function (err) {
-      console.log('Something went wrong!', err);
-      res.status(500).json({ error: 'Error generating playlist' });
+    .catch((error) => {
+      console.log('Error syncing models:', error);
+      res.status(500).json({ error: 'Error syncing models' });
     });
 });
 
