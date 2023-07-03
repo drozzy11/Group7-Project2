75f1f48d80d32f1f0ec49fd38e6c68d0c0277b82
Sydney Loggins <sydneyloggins@Sydneys-Mini.attlocal.net>
2023-07-03 13:23:46 -0500

    added controllers

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const express = require('express');
 const app = express();
 const api = require('./api');
 const sequelize = require('./config/connection');
 const { insertSongs } = require('./database');
-
+const routes = require('./controller');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
-
-const initializePassport = require('./passport-config');
-initializePassport(
-  passport, 
-  email => {  users.find(user => user.email === email),
-  id => {  users.find(user => user.id === id) }
-  });
-
-const users = [];
+const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
@@ -28,50 +23,16 @@ app.use(session({
   saveUninitialized: false
 }));
 
+const users = []; 
+
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(methodOverride('_method'));
 
-app.get('/', checkAuthenticated,  (req, res) => {
-  res.render('index.ejs', { name: req.user.name });
-});
 
-app.get('/login', (req, res) => {
-  res.render('login.ejs');
-});
+app.use(routes);
 
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}));
 
-app.get('/register', (req, res) => {
-  res.render('register.ejs');
-});
-
-app.post('/register', async (req, res) => {
-  try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-      users.push({
-          id: Date.now().toString(),
-          name: req.body.name,
-          email: req.body.email,
-          password: hashedPassword
-      });
-      res.redirect('/login');
-  }
-  catch {
-      res.redirect('/register');
-  }
-  console.log(users);
-});
-
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-      return next();
-  }
-  res.redirect('/login');
-}
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
